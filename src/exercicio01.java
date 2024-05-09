363386967e8f9218d606699b667bdee059a54f3e
erick <erickcavalhero555@gmail.com>
2024-05-08 21:45:58 -0300

    primeiros exercicios

---
+++ src/exercicio01.java
@@ -0,0 +1,22 @@
+//Faça um programa que determine e mostre os cincos primeiros  multiplos de 3,considerando numeros maiores de 0.
+
+
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        int num = 1;
+
+        while (count < 5) {
+
+            if (num % 3 == 0) {
+                // Se sim, mostramos o número e incrementamos o contador
+                System.out.println(num);
+                count++;
+            }
+
+            num++;
+        }
+    }
+}
