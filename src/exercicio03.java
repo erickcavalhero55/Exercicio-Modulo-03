de372da5873b0dcf0e03d192bac5d20a8c0519ba
erick <erickcavalhero555@gmail.com>
2024-05-09 06:02:47 -0300

    continuação dos exercicios

---
+++ src/exercicio03.java
@@ -0,0 +1,14 @@
+//Faça um algoritmo utilizando o comando while que mostra uma contagem regressiva na tela, iniciando em 10 e terminando em 0.
+//Mostrar uma mensagem Fim após a contagem
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num >=0){
+            System.out.println(num);
+            num --;
+        }
+        System.out.println("Fim");
+
+    }
+}
